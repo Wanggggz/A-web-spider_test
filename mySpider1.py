46454e84501236d480bf8d8f4c7781465a8bf8fc
sKiRe <502240842@qq.com>
2018-02-27 13:14:54 +0800

    2018_2_27
    1_做了过滤，现在可以直观显示回复数，不用一行一行的看了
    2_做了翻页，改参数就可以直接爬取多页的信息了
    明天尝试加上楼主名

--- mySpider1.py
+++ mySpider1.py
@@ -1,19 +1,29 @@
 import requests
 from lxml import etree
-
+import re
 
 
 def looks(url,rule):
     html = requests.get(url)
     selector = etree.HTML(html.text)
-    content_field = selector.xpath(rule)
-    for i in range(9):
-        print(etree.tostring(content_field[i]))
+    ref_num = selector.xpath(rule[0])
+                                                
+    
+    
+    for i in range(0,49):
+        ref_num_c = re.match(b'<span class="threadlist_rep_num center_text" title="&#x56DE;&#x590D;">(.*)</span>&#13;\n',etree.tostring(ref_num[i])).groups()
+
+        print ('回复数：',ref_num_c[0].decode())
+                                                
 
 
 
 if(__name__ == '__main__'):
-    url = 'http://tieba.baidu.com/f?kw=bilibili&fr=index&red_tag=k0670506289'
-    rule = u'//span[@class = "threadlist_rep_num center_text"]'
-    looks(url,rule)
+    page = 0
+    for i in range(2):
+        url = 'http://tieba.baidu.com/f?kw=bilibili&ie=utf-8&pn=' + str(page)
+        rule = [u'//span[@class = "threadlist_rep_num center_text"]',
+                  u'//span[@class = "tb_icon_author"]']
+        looks(url,rule)
+        page = page + 50
     
